d8b26566d5325613d0c1afe5cf6650cc28d818c5
DJ Drakos <79958858+dianajodrakos@users.noreply.github.com>
2021-06-25 09:17:58 -0700

    Revert "dev2"

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,21 +1,58 @@
 require('dotenv').config();
 
+const { execSync } = require('child_process');
+
 const fakeRequest = require('supertest');
 const app = require('../lib/app');
+const client = require('../lib/client');
 
 describe('app routes', () => {
   describe('routes', () => {
+    let token;
   
-    test('returns location data', async() => {
+    beforeAll(async () => {
+      execSync('npm run setup-db');
+  
+      await client.connect();
+      const signInData = await fakeRequest(app)
+        .post('/auth/signup')
+        .send({
+          email: 'jon@user.com',
+          password: '1234'
+        });
+      
+      token = signInData.body.token; // eslint-disable-line
+    }, 10000);
+  
+    afterAll(done => {
+      return client.end(done);
+    });
 
-      const expectation =   {
-        'formatted_query': 'Seattle, King County, Washington, USA',
-        'latitude': '47.6038321',
-        'longitude': '-122.3300624'
-      };
+    test('returns animals', async() => {
+
+      const expectation = [
+        {
+          'id': 1,
+          'name': 'bessie',
+          'cool_factor': 3,
+          'owner_id': 1
+        },
+        {
+          'id': 2,
+          'name': 'jumpy',
+          'cool_factor': 4,
+          'owner_id': 1
+        },
+        {
+          'id': 3,
+          'name': 'spot',
+          'cool_factor': 10,
+          'owner_id': 1
+        }
+      ];
 
       const data = await fakeRequest(app)
-        .get('/location')
+        .get('/animals')
         .expect('Content-Type', /json/)
         .expect(200);
 
